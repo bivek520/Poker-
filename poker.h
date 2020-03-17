25559ee837a17f482cc2822fcb5fb900edfcf0bd
benlemke <benlemke10@gmail.com>
2020-03-17 11:48:22 -0500

    First Commit

---
+++ poker.h
@@ -0,0 +1,28 @@
+#ifndef POKER_H
+#define POKER_H
+#include <vector>
+
+using namespace std;
+
+
+
+class card
+{
+public:
+    int value;
+    char suit;
+};
+
+class player
+{
+public:
+    vector<card> hand;
+};
+
+class deck
+{
+    vector<card> d;
+};
+
+
+#endif
