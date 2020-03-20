bc9a19adea4ce638031a2dc2b0b6fc09c753f14d
txt5030 <61169572+txt5030@users.noreply.github.com>
2020-03-20 13:41:20 -0500

    Create MainWindow.cpp

---
+++ MainWindow.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include "MainWindow.h"
+
+MainWindow::MainWindow()
+{
+    
+}
+void MainWindow::Refresh()
+{
+    
+}
+void MainWindow::Display_Player_Hand()
+{
+    
+}
+void MainWindow::Balance_Visibility()
+{
+    
+}
+void MainWindow::Player_Status()
+{
+    
+}
+void MainWindow::Fold_Button_Pressed()
+{
+    
+}
+void MainWindow::Match_Button_Color()
+{
+    
+}
+void MainWindow::Match_Button_Label()
+{
+    
+}
+void MainWindow::Match_Button_Pressed()
+{
+    
+}
+void MainWindow::Add_Button_Pressed()
+{
+    
+}
+void MainWindow::Exchange_Button_Label()
+{
+    
+}
+void MainWindow::Exchange_Button_Pressed()
+{
+    
+}
+void MainWindow::Status_Label()
+{
+    
+}
+void MainWindow::Status_Color()
+{
+    
+}
+void MainWindow::Status_Opacity()
+{
+    
+}
+void MainWindow::Slider_()
+{
+    
+}
+void MainWindow::SliderStatus() //or Widget
+{
+    
+}
+void MainWindow::MinAdd()
+{
+    
+}
+void MainWindow::MaxAdd()
+{
+    
+}
+void MainWindow::Balance_Opacity()
+{
+    
+}
+
+void MainWindow::Reveal_Cards()
+{
+    
+}
+void MainWindow::SelectionBoxStatus()
+{
+    
+}
+void MainWindow::TurnPosition()
+{
+    
+}
+void MainWindow::PlayerCards()
+{
+    
+}
+void MainWindow::Pot(int potValue)
+{
+    
+}
+void MainWindow::Timer()
+{
+    
+}
+void MainWindow::Id_Opacity()
+{
+    
+}
+MainWindow::~MainWindow()
+{
+}
