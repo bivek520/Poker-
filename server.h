a0c915e8be3005f7c22efa1500d57eb7f382ce2e
nirakarnepal <54406523+nirakarnepal@users.noreply.github.com>
2020-04-14 21:14:55 -0500

    still working .. check if it's correct

--- server.h
+++ server.h
@@ -9,8 +9,20 @@ class Server {
     protected:
 	void organizeHand(int hand[]); 
 	void rankHand();
+	
+	//please check if it's correct
+	int setBalance();
+	void exchange(bool exchanged, int hands[]);
+	void givePot(int winner);
+	void splitPot(int player)
+	void fold(int player)
+	void call(int currentBalance, int playerBalance);
+	void bet(int currentBalance, int playerBalance);
+
     private:
 	int potVal;
+	bool exchanged;
+	bool fold;
 };
 #endif 
 
