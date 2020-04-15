dd1d0d61f597a0113a3c8ebfb4be3fb1614ee653
bivek520 <57966410+bivek520@users.noreply.github.com>
2020-04-14 23:27:38 -0500

    Update main.cpp

--- Bivek/main.cpp
+++ Bivek/main.cpp
@@ -2,9 +2,13 @@
 
 int main(int argc, char* argv[]){
 
-  auto app = Gtk::Application::create(argc,argv,"Nirakar.test");
-  Mainwin win;
-  app->run(win);
+   auto app = Gtk::Application::create(argc,argv,"Nirakar.test");
+    
+    Gtk::Main kit(argc, argv);
+    Mainwin *win = new Mainwin();
+    win->show();
+    kit.run();
+    //app->run(*win);
 
 
 }
