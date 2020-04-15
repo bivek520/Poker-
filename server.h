aa0189e7cd880a2e89aeb82f876cac77801ae3b4
nirakarnepal <54406523+nirakarnepal@users.noreply.github.com>
2020-04-15 12:48:40 -0500

    Update server.h

--- server.h
+++ server.h
@@ -14,8 +14,8 @@ class Server {
 	int setBalance();
 	void exchange(bool exchanged, int hands[]);
 	void givePot(int winner);
-	void splitPot(int player)
-	void fold(int player, std::string playerAction)
+	void splitPot(int player);
+	void fold(int player, std::string playerAction);
 	void call(int potValue, int playerBalance, std::string playerAction);
 	void bet(int potValue, int playerBalance, std::string playerAction);
 	void raise(int potValue, int playerBalance, std::string playerAction);
