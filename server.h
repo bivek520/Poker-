887113e4329837cf829c0951f1507e1c4420b74c
nirakarnepal <54406523+nirakarnepal@users.noreply.github.com>
2020-04-14 21:25:57 -0500

    check if its correct

--- server.h
+++ server.h
@@ -15,10 +15,15 @@ class Server {
 	void exchange(bool exchanged, int hands[]);
 	void givePot(int winner);
 	void splitPot(int player)
-	void fold(int player)
-	void call(int currentBalance, int playerBalance);
-	void bet(int currentBalance, int playerBalance);
-
+	void fold(int player, std::string playerAction)
+	void call(int potValue, int playerBalance, std::string playerAction);
+	void bet(int potValue, int playerBalance, std::string playerAction);
+	void raise(int potValue, int playerBalance, std::string playerAction);
+	void All_in(int potValue, int playerBalance, std::string playerAction);
+	int minBet();
+	int maxBet();
+	int minRaise();
+	inr maxRaise();
     private:
 	int potVal;
 	bool exchanged;
