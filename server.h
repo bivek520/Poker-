cb3518b9c56bbabe2e0194b5e03d2fab50534edd
nirakarnepal <54406523+nirakarnepal@users.noreply.github.com>
2020-04-14 21:27:17 -0500

    what is this ????

--- server.h
+++ server.h
@@ -14,8 +14,8 @@ class Server {
 	int setBalance();
 	void exchange(bool exchanged, int hands[]);
 	void givePot(int winner);
-	void splitPot(int player)
-	void fold(int player, std::string playerAction)
+	void splitPot(int player);
+	void fold(int player, std::string playerAction);
 	void call(int potValue, int playerBalance, std::string playerAction);
 	void bet(int potValue, int playerBalance, std::string playerAction);
 	void raise(int potValue, int playerBalance, std::string playerAction);
@@ -23,7 +23,7 @@ class Server {
 	int minBet();
 	int maxBet();
 	int minRaise();
-	inr maxRaise();
+	int maxRaise();
     private:
 	int potVal;
 	bool exchanged;
