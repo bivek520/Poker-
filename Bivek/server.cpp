b8c4f9dd0b1e14fe897dae30c3b3d426c30e7587
bivek520 <57966410+bivek520@users.noreply.github.com>
2020-04-15 14:15:57 -0500

    Update server.cpp

--- Bivek/server.cpp
+++ Bivek/server.cpp
@@ -17,6 +17,7 @@
 #include <utility>
 #include "asio.hpp"
 #include "chat_message.hpp"
+#include "json.hpp"
 
 using namespace std;
 using asio::ip::tcp;
@@ -27,238 +28,246 @@ typedef std::deque<chat_message> chat_message_queue;
 
 //----------------------------------------------------------------------
 
-class chat_participant
+class player
 {
 public:
-  virtual ~chat_participant() {}
-  virtual void deliver(const chat_message& msg) = 0;
+    virtual ~player() {}
+    virtual void deliver(const chat_message& msg) = 0;
 };
 
-typedef std::shared_ptr<chat_participant> chat_participant_ptr;
+typedef std::shared_ptr<player> player_ptr;
 
 //----------------------------------------------------------------------
 
-class chat_room
+class poker_table
 {
 public:
-  void join(chat_participant_ptr participant)
-  {
-      cout<<"New Player has joined the room"<<endl;
-
-    participants_.insert(participant);
-    for (auto msg: recent_msgs_)                    //CSE3310 (server)  previous chat messages are sent to a client
+    void join(player_ptr participant)
     {
-      participant->deliver(msg);
-        //cout<<"chat_room join() for loop"<<endl;
+        cout<<"New Player has joined the room"<<endl;
+        
+        participants_.insert(participant);
+        for (auto msg: recent_msgs_)                    //CSE3310 (server)  previous chat messages are sent to a client
+        {
+            participant->deliver(msg);
+            //cout<<"poker_table join() for loop"<<endl;
+        }
+        //cout<<"poker_table join() done printing"<<endl;
     }
-    //cout<<"chat_room join() done printing"<<endl;
-  }
-
-  void leave(chat_participant_ptr participant)
-  {
-      //cout<<"chat_room leave()"<<endl;
-
-    participants_.erase(participant);
-  }
-
-  void deliver(const chat_message& msg)
-  {      //cout<<"chat_room deliver()"<<endl;
-
-    recent_msgs_.push_back(msg);
-    while (recent_msgs_.size() > max_recent_msgs)
-      recent_msgs_.pop_front();
-
-    for (auto participant: participants_)       //CSE3310 (server)  messages are sent to all connected clients
+    
+    void leave(player_ptr participant)
     {
-      participant->deliver(msg);
-        //cout<<"sending to all the clients"<<endl;
+        //cout<<"poker_table leave()"<<endl;
+        
+        participants_.erase(participant);
     }
-  }
-
+    
+    void deliver(const chat_message& msg)
+    {      //cout<<"poker_table deliver()"<<endl;
+        
+        recent_msgs_.push_back(msg);
+        while (recent_msgs_.size() > max_recent_msgs)
+            recent_msgs_.pop_front();
+        
+        for (auto participant: participants_)       //CSE3310 (server)  messages are sent to all connected clients
+        {
+            participant->deliver(msg);
+            //cout<<"sending to all the clients"<<endl;
+        }
+    }
+    
 private:
-  std::set<chat_participant_ptr> participants_;
-  enum { max_recent_msgs = 100 };           //CSE3310 (server) maximum number of messages are stored
-  chat_message_queue recent_msgs_;
+    std::set<player_ptr> participants_;
+    enum { max_recent_msgs = 100 };           //CSE3310 (server) maximum number of messages are stored
+    chat_message_queue recent_msgs_;
 };
 
 //----------------------------------------------------------------------
 
-class chat_session
-  : public chat_participant,
-    public std::enable_shared_from_this<chat_session>
+class poker_player
+: public player,
+public std::enable_shared_from_this<poker_player>
 {
 public:
-  chat_session(tcp::socket socket, chat_room& room)
+    poker_player(tcp::socket socket, poker_table& room)
     : socket_(std::move(socket)),
-      room_(room)
-  {
-      //cout<<"chat_session constructor"<<endl;
-  }
-
-  void start()
-  {
-      //cout<<"chat_session start()"<<endl;
-
-    room_.join(shared_from_this());
-    do_read_header();
-  }
-
-  void deliver(const chat_message& msg)
-  {
-      //cout<<"chat_session deliver()"<<endl;
-
-    bool write_in_progress = !write_msgs_.empty();
-    write_msgs_.push_back(msg);
-    if (!write_in_progress)
+    room_(room)
     {
-      do_write();
-
+        //cout<<"poker_player constructor"<<endl;
     }
-  }
-
+    
+    void start()
+    {
+        //cout<<"poker_player start()"<<endl;
+        
+        room_.join(shared_from_this());
+        do_read_header();
+    }
+    
+    void deliver(const chat_message& msg)
+    {
+        //cout<<"poker_player deliver()"<<endl;
+        
+        bool write_in_progress = !write_msgs_.empty();
+        write_msgs_.push_back(msg);
+        if (!write_in_progress)
+        {
+            do_write();
+            
+        }
+    }
+    
 private:
-  void do_read_header()
-  {
-      //cout<<"chat_session do_read_header()"<<endl;
-
-    auto self(shared_from_this());
-    asio::async_read(socket_,
-        asio::buffer(read_msg_.data(), chat_message::header_length),
-        [this, self](std::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec && read_msg_.decode_header())
+    void do_read_header()
+    {
+      auto self(shared_from_this());
+      asio::async_read(socket_,
+          asio::buffer(read_msg_.data(), chat_message::header_length),
+          [this, self](std::error_code ec, std::size_t /*length*/)
           {
-            do_read_body();
-          }
-          else
-          {
-            room_.leave(shared_from_this());
-          }
-        });
-  }
-
-  void do_read_body()
-  {
-      //cout<<"chat_session do_read_body()"<<endl;
-
-    auto self(shared_from_this());
-    asio::async_read(socket_,
-        asio::buffer(read_msg_.body(), read_msg_.body_length()),
-        [this, self](std::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec)
-          {
-            room_.deliver(read_msg_);
-            do_read_header();
-          }
-          else
-          {
-            room_.leave(shared_from_this());
-          }
-        });
-  }
-
-  void do_write()
-  {
-      //cout<<"chat_session do_write()"<<endl;
-
-    auto self(shared_from_this());
-    asio::async_write(socket_,
-        asio::buffer(write_msgs_.front().data(),
-          write_msgs_.front().length()),
-        [this, self](std::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec)
-          {
-            write_msgs_.pop_front();
-            if (!write_msgs_.empty())
+            if (!ec && read_msg_.decode_header())
             {
-              do_write();
+              do_read_body();
             }
-          }
-          else
-          {
-            room_.leave(shared_from_this());
-          }
-        });
-  }
+            else
+            {
+              room_.leave(shared_from_this());
+            }
+          });
+    }
 
-  tcp::socket socket_;
-  chat_room& room_;
-  chat_message read_msg_;
-  chat_message_queue write_msgs_;
+    void do_read_body()
+    {
+      auto self(shared_from_this());
+      asio::async_read(socket_,
+          asio::buffer(read_msg_.body(), read_msg_.body_length()),
+          [this, self](std::error_code ec, std::size_t /*length*/)
+          {
+            if (!ec)
+            {
+                nlohmann::json to_dealer = nlohmann::json::parse(std::string(read_msg_.body()));
+                nlohmann::json to_player;  // represents the entire game state.  sent to all players
+                to_player["turn"] = "3f96b414-9ac9-40b5-8007-90d0e771f0d0";   // UUID of the current player.
+                to_player["chat"] = to_dealer["chat"];
+                to_player["dealer_comment"] = "fred has raised and received 2 new cards";
+                to_player["recommended_play"] = "you should fold";
+                to_player["hand"] = {
+                    {{"bet",1},{"current_bet",10}, {"uuid","3f96b414-9ac9-40b5-8007-90d0e771f0d0"} , {"name","Bud"} ,{"cards",{"acespades","10hearts","9clubs","2diamonds","kinghearts"}}},
+                    {{"bet",2},{"current_bet",1}, {"uuid","3f96b414-9ac9-40b5-8007-20d0e771f0d0"} , {"name","Donald"} ,{"cards",{"acehearts","10spades","9clubs","2clubs","jackhearts"}}},
+                    {{"bet",5},{"current_bet",5}, {"uuid","3f96b414-9ac9-40b5-8007-30d0e771f0d0"} , {"name","Ann"} ,{"cards",{"aceclubs","10diamonds","9clubs","2hearts","queenhearts"}}},
+                    {{"bet",10},{"current_bet",0}, {"uuid","3f96b414-9ac9-40b5-8007-40d0e771f0d0"} , {"name","Melania"} ,{"cards",{"acediamonds","10clubs","9clubs","2spades","kinghearts"}}}
+                };
+                
+                std::string t = to_player.dump();
+                chat_message sending;
+                if (t.size() < chat_message::max_body_length)
+                {
+                    std::cout << "the size string being sent is " << t.size() << std::endl;
+                    memcpy( sending.body(), t.c_str(), t.size() );
+                    sending.body_length(t.size());
+                    sending.encode_header();
+                    room_.deliver(sending);
+                }
+                do_read_header();
+            }
+            else
+            {
+                room_.leave(shared_from_this());
+            }
+        });
+    }
+    
+    void do_write()
+    {
+      auto self(shared_from_this());
+      asio::async_write(socket_,
+          asio::buffer(write_msgs_.front().data(),
+            write_msgs_.front().length()),
+          [this, self](std::error_code ec, std::size_t /*length*/)
+          {
+            if (!ec)
+            {
+              write_msgs_.pop_front();
+              if (!write_msgs_.empty())
+              {
+                do_write();
+              }
+            }
+            else
+            {
+              room_.leave(shared_from_this());
+            }
+          });
+    }
+    
+    tcp::socket socket_;
+    poker_table& room_;
+    chat_message read_msg_;
+    chat_message_queue write_msgs_;
 };
 
 //----------------------------------------------------------------------
 
-class chat_server
+class poker_game
 {
 public:
-  chat_server(asio::io_context& io_context,
-      const tcp::endpoint& endpoint)
+    poker_game(asio::io_context& io_context,
+               const tcp::endpoint& endpoint)
     : acceptor_(io_context, endpoint)
-  {
-      //cout<<"chat_server constructor"<<endl;
-
-    do_accept();
-  }
-
+    {
+        //cout<<"Poker game started"<<endl;
+        
+        do_accept();
+    }
+    
 private:
-  void do_accept()
-  {
-      //cout<<"chat_server do_accept() start"<<endl;
-
-    acceptor_.async_accept(
-        [this](std::error_code ec, tcp::socket socket)
-        {
-        //cout<<"chat_server do_accept() async_accept()"<<endl;
-
-          if (!ec)
-          {
-              //cout<<"chat_server do_accept() async_accept() if brace"<<endl;
-
-            std::make_shared<chat_session>(std::move(socket), room_)->start();
-          }
-
-          do_accept();
+    void do_accept()
+    {
+        
+        
+        acceptor_.async_accept(
+                               [this](std::error_code ec, tcp::socket socket)
+                               {
+            if (!ec)
+            {
+                std::make_shared<poker_player>(std::move(socket), room_)->start();
+            }
+            
+            do_accept();
         });
-  }
-
-  tcp::acceptor acceptor_;
-  chat_room room_;
+    }
+    
+    tcp::acceptor acceptor_;
+    poker_table room_;
 };
 
 //----------------------------------------------------------------------
 
 int main(int argc, char* argv[])
 {
-  try
-  {
-      //cout<<"main->before the for loop"<<endl;
-
-    if (argc < 2)
+    try
     {
-      std::cerr << "Usage: chat_server <port> [<port> ...]\n";
-      return 1;
+        if (argc < 2)
+        {
+            std::cerr << "Usage: dealer <port> [<port> ...]\n";
+            return 1;
+        }
+        
+        asio::io_context io_context;
+        
+        std::list<poker_game> servers;
+        for (int i = 1; i < argc; ++i)
+        {
+            tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[i]));
+            servers.emplace_back(io_context, endpoint);
+        }
+        
+        io_context.run();
     }
-
-    asio::io_context io_context;
-
-    std::list<chat_server> servers;
-    for (int i = 1; i < argc; ++i)
+    catch (std::exception& e)
     {
-        //cout<<"main->for loop"<<endl;
-      tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[i]));
-      servers.emplace_back(io_context, endpoint);
+        std::cerr << "Exception: " << e.what() << "\n";
     }
-      //cout<<"main->after the for loop"<<endl;
-
-    io_context.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-
-  return 0;
+    
+    return 0;
 }
