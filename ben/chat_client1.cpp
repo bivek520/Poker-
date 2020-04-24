7e44c2c4aa8b490f3e2a091dbd7efdda7bc37512
txt5030 <61169572+txt5030@users.noreply.github.com>
2020-04-24 00:01:23 -0500

    Update chat_client1.cpp

--- ben/chat_client1.cpp
+++ ben/chat_client1.cpp
@@ -107,10 +107,10 @@ typedef std::deque<chat_message> chat_message_queue;
                          asio::buffer(read_msg_.body(), read_msg_.body_length()),
                          [this](std::error_code ec, std::size_t /*length*/)
                          {
-	gdk_threads_enter();
+	
             if (!ec)
             {
-               
+                gdk_threads_enter();
 	   	nlohmann::json from_dealer = nlohmann::json::parse(read_msg_.body());
 		nlohmann::json to_dealer = nlohmann::json::parse(read_msg_.body());
                 cout << "after parse "<<endl;
