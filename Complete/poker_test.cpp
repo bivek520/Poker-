e87a9b9f1435326cdcf9b065021b5491c13bba84
bivek520 <57966410+bivek520@users.noreply.github.com>
2020-05-10 14:43:06 -0500

    added more cases

--- Complete/poker_test.cpp
+++ Complete/poker_test.cpp
@@ -22,9 +22,7 @@ BOOST_AUTO_TEST_CASE (straight_flush)
     dealer  D;
     int hand[5]= {53,63,73,83,93};
     int rankHand[6];
-    
     D.organizeAndRank(hand,rankHand);
-    
     BOOST_CHECK(rankHand[0]==9);
     
 }
@@ -34,9 +32,7 @@ BOOST_AUTO_TEST_CASE (four_of_a_kind)
     dealer  D;
     int hand[5]= {141,142,143,144,101};
     int rankHand[6];
-    
     D.organizeAndRank(hand,rankHand);
-    
     BOOST_CHECK(rankHand[0]==8);
     
 }
@@ -49,8 +45,41 @@ BOOST_AUTO_TEST_CASE (full_house)
     int rankHand[6];
     
     D.organizeAndRank(hand,rankHand);
-    
     BOOST_CHECK(rankHand[0]==7);
+    
+}
+BOOST_AUTO_TEST_CASE (flush)
+{
+    
+    dealer  D;
+    int hand[5]= {141,21,61,91,111};
+    int rankHand[6];
+    
+    D.organizeAndRank(hand,rankHand);
+    BOOST_CHECK(rankHand[0]==6);
+    
+}
+
+BOOST_AUTO_TEST_CASE (straight)
+{
+    
+    dealer  D;
+    int hand[5]= {141,132,123,114,102};
+    int rankHand[6];
+    
+    D.organizeAndRank(hand,rankHand);
+    BOOST_CHECK(rankHand[0]==5);
+    
+}
+BOOST_AUTO_TEST_CASE (three_of_a_kind)
+{
+    
+    dealer  D;
+    int hand[5]= {141,142,143,62,53};
+    int rankHand[6];
+    
+    D.organizeAndRank(hand,rankHand);
+    BOOST_CHECK(rankHand[0]==4);
     
 }
 BOOST_AUTO_TEST_CASE (two_pair)
@@ -61,14 +90,31 @@ BOOST_AUTO_TEST_CASE (two_pair)
     int rankHand[6];
     
     D.organizeAndRank(hand,rankHand);
-    
     BOOST_CHECK(rankHand[0]==3);
+}
+BOOST_AUTO_TEST_CASE (one_pair)
+{
     
+    dealer  D;
+    int hand[5]= {141,142,81,124,132};
+    int rankHand[6];
+    
+    D.organizeAndRank(hand,rankHand);
+    BOOST_CHECK(rankHand[0]==2);
+}
+BOOST_AUTO_TEST_CASE (high_card)
+{
+    
+    dealer  D;
+    int hand[5]= {141,122,113,64,53};
+    int rankHand[6];
+    
+    D.organizeAndRank(hand,rankHand);
+    BOOST_CHECK(rankHand[0]==1);
 }
 
 BOOST_AUTO_TEST_CASE (legalDeck)
 {
-    
     dealer  D;
     D.createDeck();
     
@@ -85,8 +131,6 @@ BOOST_AUTO_TEST_CASE (legalDeck)
             BOOST_CHECK(it != Deck.end());
         }
     }
-    
-    
 }
 
 BOOST_AUTO_TEST_SUITE_END( )
